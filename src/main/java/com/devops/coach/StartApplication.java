b10cf8e062b1419b2d0477c8e7fc5655b7a8d65d
anandu raj <anandu.raj@urolime.com>
2024-01-12 10:57:39 +0530

    edited java application

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "AZURE POC");
+        model.addAttribute("title", "AZURE POC pipline");
         model.addAttribute("msg", "Hello All..We are testing springboot deployment with Helm and Jenkins Pipeline!!!!");
         return "index";
     }
